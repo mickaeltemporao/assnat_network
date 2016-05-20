9d1b4252bf921b1ed2745c5951b3864bbab27638
mickaeltemporao <mickael.temporao.1@ulaval.ca>
2016-05-20 14:20:17 -0400

    init scraper

---
+++ assnat_scraper.R
@@ -0,0 +1,51 @@
+#!/usr/bin/env Rscript
+# ------------------------------------------------------------------------------
+# Title:        TODO: (add title)
+# Filename:     assnat_scraper.R
+# Description:  TODO: (write me)
+# Version:      0.0.0.000
+# Created:      2016-05-20 14:19:50
+# Modified:     2016-05-20 14:19:54
+# Author:       Mickael Temporão < mickael.temporao.1 at ulaval.ca >
+# ------------------------------------------------------------------------------
+# Copyright (C) 2016 Mickael Temporão
+# Licensed under the GPL-2 < https://www.gnu.org/licenses/gpl-2.0.txt >
+# ------------------------------------------------------------------------------
+
+library(rvest)
+base <- 'http://www.assnat.qc.ca'
+url <- read_html('http://www.assnat.qc.ca/fr/membres/notices/index.html')
+letters <- url %>%
+  html_nodes("p a") %>%
+  html_attr('href')
+letters <- letters[2:length(letters)]
+
+pages <- data.frame()
+#i <- letters[1]
+for (i in letters) {
+  pages[i,] <- read_html(paste0(base,i))
+}
+
+test <- url %>%
+  html_nodes('//*[@id="Wrap"]/div[8]')
+
+
+  html_text() %>%
+  as.numeric()
+rating
+#> [1] 7.8
+
+cast <- lego_movie %>%
+  html_nodes("#titleCast .itemprop span") %>%
+  html_text()
+cast
+#>  [1] "Will Arnett"     "Elizabeth Banks" "Craig Berry"
+#>  [4] "Alison Brie"     "David Burrows"   "Anthony Daniels"
+#>  [7] "Charlie Day"     "Amanda Farinos"  "Keith Ferguson"
+#> [10] "Will Ferrell"    "Will Forte"      "Dave Franco"
+#> [13] "Morgan Freeman"  "Todd Hansen"     "Jonah Hill"
+
+poster <- lego_movie %>%
+  html_nodes("#img_primary img") %>%
+  html_attr("src")
+poster
