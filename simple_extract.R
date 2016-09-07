96055ad3cce2295d81fb872e9adf95777779b9ac
Mickael Temporão <mickael.temporao.1@ulaval.ca>
2016-09-07 12:00:38 -0400

    added src + party file

--- simple_extract.R
+++ simple_extract.R
@@ -5,7 +5,7 @@
 # Description:  TODO: (write me)
 # Version:      0.0.0.000
 # Created:      2016-09-07 06:32:53
-# Modified:     2016-09-07 08:48:24
+# Modified:     2016-09-07 10:00:34
 # Author:       Mickael Temporão < mickael.temporao.1 at ulaval.ca >
 # ------------------------------------------------------------------------------
 # Copyright (C) 2016 Mickael Temporão
@@ -17,6 +17,21 @@ library(stringr)
 
 test <- read_html("http://www.assnat.qc.ca/fr/deputes/papineau-louis-joseph-4735/biographie.html")
 test <- read_html(output$mp_url[2540])
+
+to_plain <- function(s) {
+# Converts all accented characters to plain text
+# http://stackoverflow.com/questions/17517319/r-replacing-foreign-characters-in-a-string
+   # 1 character substitutions
+   old1 <- "šžþàáâãäåçèéêëìíîïðñòóôõöùúûüý"
+   new1 <- "szyaaaaaaceeeeiiiidnooooouuuuy"
+   s1 <- chartr(old1, new1, s)
+   # 2 character substitutions
+   old2 <- c("œ", "ß", "æ", "ø")
+   new2 <- c("oe", "ss", "ae", "oe")
+   s2 <- s1
+   for(i in seq_along(old2)) s2 <- gsub(old2[i], new2[i], s2, fixed = TRUE)
+   s2
+}
 
 # ID
 # NAME
@@ -42,7 +57,7 @@ get_gender(test)
 # YOB & YOD
 get_year <- function (x, type) {
 # Returns a dataframe with yob & yod of selected mep
-  x <- get_desc(test)
+  x <- get_desc(x)
   sentences <- unlist(strsplit(x, ". ", fixed=T))
   if (type == 1){
   yob_sent <- sentences[grep('Né', sentences)]
@@ -75,5 +90,30 @@ get_year <- function (x, type) {
 get_year(test, 2)
 
 # PARTY
+source("src/party.R")
+get_party <- function (x) {
+  x <- test
+  x <- tolower(get_desc(x))
+  x <- to_plain(x)
+  x <- sentences <- unlist(strsplit(x, ". ", fixed=T))
+  for (i in party) {
+    if (grep(i, x, fixed=TRUE)>=0) {
+      grep(i, x, fixed=TRUE
+
+yod_sent <- sentences[grep('Décédé', sentences)]
+      yod <- as.numeric(
+        grep('[0-9]{4}',
+          gsub(',','',
+            unlist(
+              strsplit(yod_sent, split=' '))),
+          value=T))
+    }
+    else (identical(grep(i, x, fixed=TRUE), integer(0))) {
+    0
+    }
+
+
+  }
+}
 
 # LINKS
---
+++ src/party.R
@@ -0,0 +1,86 @@
+#!/usr/bin/env Rscript
+# ------------------------------------------------------------------------------
+# Title:        TODO: (add title)
+# Filename:     party.R
+# Description:  TODO: (write me)
+# Version:      0.0.0.000
+# Created:      2016-09-07 09:12:22
+# Modified:     2016-09-07 09:46:34
+# Author:       Mickael Temporão < mickael.temporao.1 at ulaval.ca >
+# ------------------------------------------------------------------------------
+# Copyright (C) 2016 Mickael Temporão
+# Licensed under the GPL-2 < https://www.gnu.org/licenses/gpl-2.0.txt >
+# ------------------------------------------------------------------------------
+# Political Parties
+# adq = 'Action démocratique du Québec',
+# aln = 'Action libérale nationale',
+# bpc = 'Bloc populaire canadien',
+# caq = 'Coalition Avenir Québec',
+# dem = 'Les démocrates',
+# fcc = 'Fédération du Commonwealth Coopératif',
+# lnc = 'Ligue nationaliste canadienne',
+# pc  = 'Parti Canadien',
+# pcq = 'Parti conservateur du Québec',
+# pcr = 'Parti créditiste',
+# pdc = 'Parti démocrate créditiste',
+# pe  = 'Parti égalité',
+# plq = 'Parti libéral du Québec',
+# pnp = 'Parti national populaire',
+# po  = 'Parti ouvrier',
+# pp  = 'Parti Patriote',
+# pq  = 'Parti québécois',
+# psd = 'Parti social démocratique du Québec',
+# qs  = 'Québec Solidaire',
+# rcq = 'Ralliement créditiste du Québec',
+# un  = 'Union nationale',
+# uq  = 'Unité-Québec',
+
+party <- c(
+  'action democratique du quebec',
+  'action liberale nationale',
+  'bloc populaire canadien',
+  'coalition avenir quebec',
+  'les democrates',
+  'federation du commonwealth cooperatif',
+  'ligue nationaliste canadienne',
+  'parti canadien',
+  'parti conservateur du quebec',
+  'parti creditiste',
+  'parti democrate creditiste',
+  'parti egalite',
+  'parti liberal du quebec',
+  'parti national populaire',
+  'parti ouvrier',
+  'parti patriote',
+  'parti quebecois',
+  'parti social democratique du quebec',
+  'quebec solidaire',
+  'ralliement creditiste du quebec',
+  'union nationale',
+  'unite-quebec'
+)
+
+abv_party <- c(
+  'adq',
+  'aln',
+  'bpc',
+  'caq',
+  'dem',
+  'fcc',
+  'lnc',
+  'pc' ,
+  'pcq',
+  'pcr',
+  'pdc',
+  'pe' ,
+  'plq',
+  'pnp',
+  'po' ,
+  'pp' ,
+  'pq' ,
+  'psd',
+  'qs' ,
+  'rcq',
+  'un' ,
+  'uq'
+)
