23fa07233a94957cd14c18579614037e7f7f4217
Mickael Temporão <mickael.temporao.1@ulaval.ca>
2016-09-08 08:57:18 -0400

    major cleanup

--- simple_extract.R
+++ simple_extract.R
@@ -5,7 +5,7 @@
 # Description:  TODO: (write me)
 # Version:      0.0.0.000
 # Created:      2016-09-07 06:32:53
-# Modified:     2016-09-07 12:47:07
+# Modified:     2016-09-08 08:56:58
 # Author:       Mickael Temporão < mickael.temporao.1 at ulaval.ca >
 # ------------------------------------------------------------------------------
 # Copyright (C) 2016 Mickael Temporão
@@ -14,9 +14,11 @@
 library(quanteda)
 library(rvest)
 library(stringr)
-
-test1 <- read_html("http://www.assnat.qc.ca/fr/deputes/papineau-louis-joseph-4735/biographie.html")
-test2 <- read_html(output$mp_url[2540])
+library(pbapply)
+# ID
+# NAME
+# URL
+test <- read_html("http://www.assnat.qc.ca/fr/deputes/papineau-louis-joseph-4735/biographie.html")
 
 to_plain <- function(s) {
 # Converts all accented characters to plain text
@@ -30,12 +32,9 @@ to_plain <- function(s) {
    new2 <- c("oe", "ss", "ae", "oe")
    s2 <- s1
    for(i in seq_along(old2)) s2 <- gsub(old2[i], new2[i], s2, fixed = TRUE)
-   s2
+   return(s2)
 }
 
-# ID
-# NAME
-# URL
 # DESCRIPTION
 get_desc <- function (x) {
   desc_str <- '.imbGauche'
@@ -88,7 +87,7 @@ get_year <- function (x, type) {
   print(paste0('Choose between: 1 = Year of Birth; 2 = Year of Death'))
   }
 }
-get_year(test, 2)
+get_year(test, 1)
 
 # PARTY
 source("src/party.R")
@@ -96,19 +95,70 @@ get_party <- function (x) {
 # Returns a data.frame of parties
   x <- tolower(get_desc(x))
   x <- to_plain(x)
-  x <- sentences <- unlist(strsplit(x, ". ", fixed=T))
+  x <- unlist(strsplit(x, ". ", fixed=T))
   mp_parties <- as.data.frame(setNames(replicate(length(party),numeric(0), simplify = F), abv_party))
   for (i in party) {
     mp_parties[1,match(i,party)]  <- ifelse(identical(grep(i, x, fixed=TRUE), integer(0)),0,1)
   }
   return(mp_parties)
 }
-get_party(test2)
+get_party(test)
+
+# Temporary mp_names and id creation
+output <- read.csv('data/output.csv', stringsAsFactors=F)
+mp_id <- paste0('mp_', 1:length(output$mp_name))
+mp_names <- tolower(output$mp_name)
+mp_names <- to_plain(mp_names)
+mp_names <- gsub(',.', ',', mp_names)
+for (i in 1:length(mp_names)) {
+  mp_names[i] <- paste(unlist(str_split(mp_names[i], ","))[-1], unlist(str_split(mp_names[i], ","))[1])
+}
+
+# mp_names <- NULL
+# mp_names <- character(length(output$mp_name))
+# for (i in 1:length(mp_names)) {
+#   if (identical(grep('patrimoine', output$mp_url[i]), integer(0))) {
+#     mp_names[i] <- output$mp_url[i] %>% read_html %>%
+#     html_nodes('.enteteFicheDepute h1') %>%
+#     html_text
+#   } else {
+#     mp_names[i] <- output$mp_url[i] %>% read_html %>%
+#     html_nodes('.imbGauche h1') %>%
+#     html_text
+#   }
+# }
 
 # LINKS
 source('src/links.R')
 get_links <- function (x) {
-
+#  x <- test
+#  x <- read_html(output$mp_url[23])
+  x1 <- tolower(get_desc(x))
+  x1 <- to_plain(x1)
+  x1 <- unlist(strsplit(x1, ". ", fixed=T))
+  sentences <- unlist(grep(paste(links, collapse='|'), x1))
+  urls <- x %>%
+    html_nodes("p a") %>%
+    html_attr('href') %>%
+    .[-1]
+  mps <- as.data.frame(setNames(replicate(length(mp_names),numeric(0), simplify = F), mp_id), stringsAsFactors=F)
+  for (i in 1:length(urls)) {
+    which_mp <- mp_id[which(urls[i] == output$mp_url)]
+    mps[1,which_mp] <- 1
+  }
+  return(mps)
 }
+get_links(test)
 
-unlist(str_extract_all(x, paste(party, collapse='|')))
+# for (i in sentences) {
+#   link <- unlist(str_extract_all(x[i], paste(links, collapse='|')))
+
+########
+
+########
+
+source("src/party.R")
+mp_parties <- as.data.frame(setNames(replicate(length(party),numeric(0), simplify = F), abv_party))
+output <- dplyr::bind_rows(output, mp_parties)
+out <- as.data.frame(setNames(replicate(length(mp_names),character(0), simplify = F), output$mp_id), stringsAsFactors=F)
+output <- dplyr::bind_rows(output, out)
